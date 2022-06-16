918e85f6c6158ea1f1e6c1cb90ad4b808028552a
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-06-16 22:31:19 +0700

    golang
    learn about how to make array in golang

---
+++ array.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var value = [3]string{
+		"Bangkit",
+		"Juang",
+		"Raharjo",
+	}
+
+	// value[0] = "Bangkit"
+	// value[1] = "Juang"
+	// value[2] = "Raharjo"
+
+	fmt.Println(value)
+}
---
+++ comparation-operator.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nama1 = "Juang"
+	var nama2 = "Juang"
+
+	var result = nama1 == nama2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
