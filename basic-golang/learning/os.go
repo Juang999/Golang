f5678dbc472b9a827c743013847f277f15497478
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-10-10 16:35:10 +0700

    new learning
    new learning

--- basic-golang/learning/os.go
+++ basic-golang/learning/os.go
@@ -8,4 +8,12 @@ import (
 func main(){
 	args := os.Args
 	fmt.Println(args)
+
+	name, err := os.Hostname()
+
+	if err == nil {
+		fmt.Println(name)
+	} else {
+		fmt.Println("error: ", err.Error())
+	}
 }
